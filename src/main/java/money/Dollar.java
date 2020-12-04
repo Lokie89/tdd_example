5ba76195e0f28edb87813bf9838893ad969acb69
Lokie89 <traeuman@gmail.com>
2020-12-04 14:43:33 +0900

    refactor : multiplication test 초록불 완성

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -4,11 +4,11 @@ public class Dollar {
 
     int amount;
 
-    public Dollar(int amount){
+    public Dollar(int amount) {
         this.amount = amount;
     }
 
-    void times(int multiplier){
-        amount = amount * multiplier;
+    void times(int multiplier) {
+        amount *= multiplier;
     }
 }
