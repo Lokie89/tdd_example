1e69b58a2d0db341450014b338336607f0541e3c
Lokie89 <traeuman@gmail.com>
2020-12-04 14:11:23 +0900

    test : Test 실행 -> expected: <10> but was: <0>, 실패

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -1,9 +1,12 @@
 package money;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
 
+    @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         five.times(2);
