94db8c66519804f8d673f7021f1efbd8b25001b5
Lokie89 <traeuman@gmail.com>
2020-12-05 20:59:53 +0900

    feat : Money Dollar 팩터리 메서드 추가, 테스트 성공

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -3,6 +3,10 @@ package money;
 public class Money {
     protected int amount;
 
+    static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
+
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
