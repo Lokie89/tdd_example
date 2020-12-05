7320a1e3334e633d11d5dd331576f504021ff850
Lokie89 <traeuman@gmail.com>
2020-12-05 21:27:40 +0900

    refactor : Money abstract class 로 변경, Money times abstract 메서드 추가, 테스트 성공

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,11 +1,13 @@
 package money;
 
-public class Money {
+public abstract class Money {
     protected int amount;
 
     static Dollar dollar(int amount){
         return new Dollar(amount);
     }
+
+    abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object obj) {
