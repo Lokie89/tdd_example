7608f91253d7f37b2dd0d7b795f8d3476ec79f89
Lokie89 <traeuman@gmail.com>
2020-12-08 15:59:43 +0900

    feat : Pair class 추가, 환율표를 위함

---
+++ src/main/java/money/Pair.java
@@ -0,0 +1,11 @@
+package money;
+
+public class Pair {
+    private String from;
+    private String to;
+
+    Pair(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+}
