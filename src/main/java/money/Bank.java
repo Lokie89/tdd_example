d15cd0eb68eb0da46e3b123ef8e3e3428d1256bc
Lokie89 <traeuman@gmail.com>
2020-12-08 16:07:57 +0900

    feat : Bank rates 환율을 담당하는 필드 추가, addRate 메서드 추가, 테스트 통과

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -1,6 +1,11 @@
 package money;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
+
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
@@ -9,5 +14,9 @@ public class Bank {
         return (from.equals("CHF") && to.equals("USD"))
                 ? 2
                 : 1;
+    }
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 }
