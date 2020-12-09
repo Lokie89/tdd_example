8258d291f101214fedbb712771dd964bb77b68e1
Lokie89 <traeuman@gmail.com>
2020-12-09 14:58:00 +0900

    feat : Array sort 메서드 추가, 테스트 성공

--- src/main/java/algorithm/knumber/Array.java
+++ src/main/java/algorithm/knumber/Array.java
@@ -12,4 +12,9 @@ public class Array {
     int[] cut(int[] cutIndex) {
         return Arrays.copyOfRange(arrays, cutIndex[0] - 1, cutIndex[1]);
     }
+
+    int[] sort(){
+        Arrays.sort(arrays);
+        return arrays;
+    }
 }
