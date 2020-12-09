f368feeb797c020b80d482ca3870cae5ab4c897b
Lokie89 <traeuman@gmail.com>
2020-12-09 15:02:00 +0900

    test : cutSortArray test 추가, cut sort 메서드 체이닝 적용, 테스트 실패

--- src/test/java/algorithm/knumber/ArrayTest.java
+++ src/test/java/algorithm/knumber/ArrayTest.java
@@ -22,4 +22,10 @@ public class ArrayTest {
         Array array = new Array(new int[]{1, 5, 2, 6, 3, 7, 4});
         assertTrue(Arrays.equals(array.sort(), new int[]{1, 2, 3, 4, 5, 6, 7}));
     }
+
+    @Test
+    void testCutSortArray() {
+        Array array = new Array(new int[]{1, 5, 2, 6, 3, 7, 4});
+        array.cut(new int[]{1, 3}).sort();
+    }
 }
