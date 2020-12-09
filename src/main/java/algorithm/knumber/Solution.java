6277d4fd20d458fde2ee70b41da0e8277b5f207c
Lokie89 <traeuman@gmail.com>
2020-12-09 15:49:53 +0900

    feat : Solution 클래스 추가, solution 메서드 추가, 테스트 성공

--- src/main/java/algorithm/knumber/Solution.java
+++ src/main/java/algorithm/knumber/Solution.java
@@ -1,2 +1,13 @@
-package algorithm.knumber;public class Solution {
+package algorithm.knumber;
+
+public class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        Array cutSortGetArray = Array.of(array);
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            answer[i] = cutSortGetArray.cut(commands[i][0],commands[i][1]).sort().get(commands[i][2]);
+        }
+        return answer;
+    }
 }
