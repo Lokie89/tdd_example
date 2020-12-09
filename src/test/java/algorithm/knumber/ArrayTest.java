2b110464e22b6356c22a2a6189e7cac07ed8431e
Lokie89 <traeuman@gmail.com>
2020-12-09 15:29:42 +0900

    test : cutSortGetArray test 추가, 테스트 성공

--- src/test/java/algorithm/knumber/ArrayTest.java
+++ src/test/java/algorithm/knumber/ArrayTest.java
@@ -31,4 +31,12 @@ public class ArrayTest {
         Array array = Array.of(1, 5, 2, 6, 3, 7, 4);
         assertEquals(array.get(3), 2);
     }
+
+    @Test
+    void testCutSortGetArray() {
+        Array array = Array.of(1, 5, 2, 6, 3, 7, 4);
+        assertEquals(array.cut(2, 5).sort().get(3), 5);
+        assertEquals(array.cut(4, 4).sort().get(1), 6);
+        assertEquals(array.cut(1, 7).sort().get(3), 3);
+    }
 }
