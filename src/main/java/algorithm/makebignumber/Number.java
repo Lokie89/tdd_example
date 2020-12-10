4fda741bd94c9bf659a17a15c2361c1efef47c1e
Lokie89 <traeuman@gmail.com>
2020-12-10 11:15:54 +0900

    feat : Number deleteOne 메서드 추가, delete 메서드 수정, 테스트 성공

--- src/main/java/algorithm/makebignumber/Number.java
+++ src/main/java/algorithm/makebignumber/Number.java
@@ -1,12 +1,32 @@
 package algorithm.makebignumber;
 
+import java.util.HashSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Number {
 
-    Number(String number) {
+    String number;
 
+    Number(String number) {
+        this.number = number;
     }
 
     int[] delete(int count) {
-        return new int[]{3, 5};
+        return deleteOne(number).stream().mapToInt(value -> value).toArray();
+    }
+
+    private HashSet<Integer> deleteOne(String number) {
+        HashSet<Integer> numberSet = new HashSet<>();
+        char[] numberCharArray = number.toCharArray();
+        for (int i = 0; i < numberCharArray.length; i++) {
+            int finalI = i;
+            String removedNumber = IntStream.range(0, numberCharArray.length)
+                    .filter(index -> finalI != index)
+                    .mapToObj(index -> String.valueOf(numberCharArray[index]))
+                    .collect(Collectors.joining());
+            numberSet.add(Integer.parseInt(removedNumber));
+        }
+        return numberSet;
     }
 }
