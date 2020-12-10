da456adbffe57d689115d16c9d11da950de46cb7
Lokie89 <traeuman@gmail.com>
2020-12-10 09:38:50 +0900

    feat : Number class, 생성자, delete 메서드, 테스트 성공

--- src/main/java/algorithm/makebignumber/Number.java
+++ src/main/java/algorithm/makebignumber/Number.java
@@ -1,2 +1,12 @@
-package algorithm.makebignumber;public class Number {
+package algorithm.makebignumber;
+
+public class Number {
+
+    Number(String number) {
+
+    }
+
+    int[] delete(int count) {
+        return new int[]{12, 14, 19, 24, 92, 94};
+    }
 }
