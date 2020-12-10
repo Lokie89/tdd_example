fbadc89406c7ebc6405b32c173179d6ef64ca109
Lokie89 <traeuman@gmail.com>
2020-12-10 09:40:51 +0900

    test : deleteNumber test 추가, 테스트 실패

--- src/test/java/algorithm/makebignumber/NumberTest.java
+++ src/test/java/algorithm/makebignumber/NumberTest.java
@@ -14,5 +14,8 @@ public class NumberTest {
         Number number = new Number("1924");
         int[] deletedNumbers = number.delete(2);
         assertTrue(Arrays.equals(deletedNumbers, new int[]{12, 14, 19, 24, 92, 94}));
+        Number number2 = new Number("3333");
+        int[] deletedNumbers2 = number2.delete(2);
+        assertTrue(Arrays.equals(deletedNumbers2, new int[]{33}));
     }
 }
