82818ce6dff7e052d7db1a36e2ee73779e516e4b
Lokie89 <traeuman@gmail.com>
2020-12-15 17:06:55 +0900

    feat : BaseConverter convert 메서드 수정, 테스트 성공

--- src/main/java/algorithm/nation124/BaseConverter.java
+++ src/main/java/algorithm/nation124/BaseConverter.java
@@ -9,7 +9,26 @@ public class BaseConverter {
     }
 
     Base convert(int base, int number) {
-        return null;
+        if (this.base == base) {
+            return new Base(base, number);
+        }
+        int based10 = based10(base, number);
+        StringBuilder sb = new StringBuilder();
+        while (based10 > 0) {
+            sb.append(based10 % this.base);
+            based10 /= this.base;
+        }
+        sb.reverse();
+        return new Base(this.base, Integer.parseInt(sb.toString()));
+    }
+
+    private int based10(int base, int number) {
+        char[] chars = String.valueOf(number).toCharArray();
+        int based10 = 0;
+        for (int i = chars.length - 1; i >= 0; i--) {
+            based10 += (chars[i] - 48) * Math.pow(base, i);
+        }
+        return based10;
     }
 
 }
