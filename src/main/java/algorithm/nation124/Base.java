df34e5d2cc52e744704a1abe46ebb8451079a827
Lokie89 <traeuman@gmail.com>
2020-12-15 16:32:51 +0900

    feat : BaseConverter, Base class, 테스트 실패

---
+++ src/main/java/algorithm/nation124/Base.java
@@ -0,0 +1,18 @@
+package algorithm.nation124;
+
+public class Base {
+
+    private int base;
+    private int number;
+
+    Base(int base, int number) {
+        this.base = base;
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Base base = (Base) obj;
+        return this.base == base.base && this.number == base.number;
+    }
+}
---
+++ src/main/java/algorithm/nation124/BaseConverter.java
@@ -0,0 +1,15 @@
+package algorithm.nation124;
+
+public class BaseConverter {
+
+    private int base;
+
+    BaseConverter(int base) {
+        this.base = base;
+    }
+
+    Base convert(int base, int number) {
+        return null;
+    }
+
+}
