0c8b7f417e8d435377f1adaee70b652d53dc0d44
Lokie89 <traeuman@gmail.com>
2020-12-15 16:31:39 +0900

    test : BaseConverter test 추가, 테스트 실패

---
+++ src/test/java/algorithm/nation124/BaseConverterTest.java
@@ -0,0 +1,16 @@
+package algorithm.nation124;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12899?language=java
+public class BaseConverterTest {
+
+    @Test
+    void convert() {
+        BaseConverter baseConverter = new BaseConverter(3);
+        Base base = baseConverter.convert(10, 5);
+        assertEquals(base, new Base(3, 12));
+    }
+}
