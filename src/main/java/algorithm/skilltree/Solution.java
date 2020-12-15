83f651f4f727c999010cf95d94d0e5e5dc97cf52
Lokie89 <traeuman@gmail.com>
2020-12-15 14:23:00 +0900

    feat : Solution solution 메서드 추가, 테스트 성공

--- src/main/java/algorithm/skilltree/Solution.java
+++ src/main/java/algorithm/skilltree/Solution.java
@@ -1,7 +1,13 @@
 package algorithm.skilltree;
 
+import java.util.Arrays;
+
 public class Solution {
     int solution(String skill, String[] skill_trees) {
-        return 2;
+        Skill s = new Skill(skill);
+        return (int) Arrays.stream(skill_trees).map(SkillTree::new).filter(skillTree -> {
+            skillTree.excludeUnskilled(s);
+            return skillTree.isCorrectOrder(s);
+        }).count();
     }
 }
