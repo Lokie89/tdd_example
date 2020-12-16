cb07cf7fc26e4f932e3865b161918580518265fd
Lokie89 <traeuman@gmail.com>
2020-12-16 16:45:26 +0900

    feat : Solution class, solution 메서드 추가, 테스트 성공

--- src/main/java/algorithm/nation124/Solution.java
+++ src/main/java/algorithm/nation124/Solution.java
@@ -1,2 +1,29 @@
-package algorithm.nation124;public class Solution {
+package algorithm.nation124;
+
+public class Solution {
+
+    String solution(int n) {
+        if (n % 3 == 0) {
+            n -= 3;
+        }
+        BaseConverter baseConverter = new BaseConverter(3);
+        Base convertBase = baseConverter.convert(10, n);
+        String numberStr = String.valueOf(convertBase.getNumber());
+        char[] numberStrCharArray = numberStr.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char num : numberStrCharArray) {
+            switch ((num - 48)) {
+                case 0:
+                    sb.append("4");
+                    break;
+                case 1:
+                    sb.append("1");
+                    break;
+                case 2:
+                    sb.append("2");
+                    break;
+            }
+        }
+        return sb.toString();
+    }
 }
